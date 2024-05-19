65513fe984d08b004b9433ef7f5b977427470a99
RAJ PRATAP <rajpratap@Rajs-MacBook-Air.local>
2024-05-19 22:13:56 +0530

    Installation of React using Vite

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,13 @@
+import Chai from "./chai";
+function App() {
+
+  return (
+    <>
+      <Chai />
+      <h1>Chai Aur React</h1>
+      <p>Learning React</p>
+    </>
+  );
+}
+
+export default App
