b33199831c298c5922ba7c827be80921f7a067f8
RAJ PRATAP <rajpratap@Rajs-MacBook-Air.local>
2024-05-19 23:11:13 +0530

    Updated after learning Custom React

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,10 +1,11 @@
 import Chai from "./chai";
 function App() {
+  const username = "Raj"
 
   return (
     <>
       <Chai />
-      <h1>Chai Aur React</h1>
+      <h1>Chai Aur React {username}</h1>
       <p>Learning React</p>
     </>
   );
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,9 +2,38 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+  return(
+    <div>
+      <h1>Custom App</h1>
+    </div>
+  )
+}
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// const ReactElement = {
+//   type: "a",
+//   props: {
+//     href: "https://google.com",
+//     target: "_blank",
+//   },
+//   children: "Click me to visit google",
+// };
 
-  <App />
-
+const AnotherElement = (
+  <a href="https://google.com" target='_blank'>Click me to visit google</a>
 )
+
+const AnotherUser = "Raj Pratap"
+
+const ReactElement = React.createElement(
+  'a',
+  {href: "https://google.com", target: "_blank"},
+  "Click me to visit google",
+  AnotherUser
+)
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  // AnotherElement 
+  // ReactElement
+  <App/>
+);
